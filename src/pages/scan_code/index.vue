e0a1d1414db2271d8ace779ad1ee9f85f36c2d3a
pingfei <pingfei1224@163.com>
2019-02-26 11:05:14 +0800

    scan-code

---
+++ src/pages/scan_code/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="scan-code">
+    <div></div>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+<style scoped lang="scss">
+</style>
